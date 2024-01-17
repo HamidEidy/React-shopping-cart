62386cfe51ccb5b1d98cefdbfcaae5b575075589
HAM!DREZA <hamidreza.eidy1999@gmail.com>
2024-01-17 12:35:43 +0330

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import store from "./redux/store";
+import { Provider } from "react-redux"
+import Products from "./pages/Products";
+import ShoppingCart from "./pages/ShoppingCart";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Provider store={store}>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+          </Routes>
+        </Provider>
       </BrowserRouter>
     </>
   );
